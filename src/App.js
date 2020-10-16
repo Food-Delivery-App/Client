0b49b0a2a5002ee5af454630e3e579cf7bfcc69b
Venkatesh S <contactvenkatesh17@gmail.com>
2020-10-16 12:39:45 +0530

    balaji commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn thozhil
+          Learn thozhil sir
         </a>
       </header>
     </div>
