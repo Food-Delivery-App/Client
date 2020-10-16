0db4840c42c5d7671cbc092473c8fdeb82743320
Venkatesh S <contactvenkatesh17@gmail.com>
2020-10-16 13:26:34 +0530

    balaji commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn thozhil sir Balaji w
+          Learn thozhil sir venkat is k2k2
         </a>
       </header>
     </div>
