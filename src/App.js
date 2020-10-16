c4cc8bb018332d645486d18677ef49f27cf0c545
venkatesh1702 <contactvenkatesh17@gmail.com>
2020-10-16 18:31:21 +0530

    commit venkat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import TopNavBar from './Components/topNavbar/topNavbar';
-import SideBar from './Components/sidebar/sidebar';
+// import TopNavBar from './Components/topNavbar/topNavbar';
+// import SideBar from './Components/sidebar/sidebar';
 import Lander from './Components/lander/lander';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <TopNavBar />
-      <SideBar />
+      {/* <TopNavBar /> */}
+      {/* <SideBar /> */}
       <Lander />
     </div>
   ); 
