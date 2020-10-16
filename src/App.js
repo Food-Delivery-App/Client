6dc23e76423ce4ea0ccf51e4470e62237689081d
Prasanth Sankar <prasanthsankar32@gmail.com>
2020-10-16 16:16:12 +0530

    pra

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Kiruba Kiruba hi</h1>
-      <h1>balaji</h1>
-      <h1>Venkat</h1>
-      <h6>Prasanth </h6>
+      <h6>balaji </h6>
       <Test />
     </div>
   ); 
