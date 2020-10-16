2c20a5bf2fdb613b22cc0e7d014c81f6b9acc2ba
Venkatesh S <contactvenkatesh17@gmail.com>
2020-10-16 13:57:52 +0530

    kiruba kiruba kiruba

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn thozhil sir venkat is k2k2
-          <p>Hi guys i am balaji</p> 
-        </a>
-      </header>
+      <h1>Kiruba</h1>
     </div>
   );
 }
