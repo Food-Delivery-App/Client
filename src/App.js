cb5de62ddba209a35dc65290d16580fc48b8f8e1
venkatesh1702 <contactvenkatesh17@gmail.com>
2020-10-19 11:20:03 +0530

    commit venkatOct19

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React, { Component } from 'react';
 import TopNavBar from './Components/topNavbar/topNavbar';
 import SideBar from './Components/sidebar/sidebar';
 import Lander from './Components/lander/lander';
+import Footer from './Components/footer/footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-
+ 
 function App() {
   return (
-      <main>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/" component={Lander} exact />
-              <Route path="/sidebar" component={SideBar} />
-              <Route path="/topnavbar" component={TopNavBar} />
-              <Route component={Error} />
-            </Switch>
-          </BrowserRouter>
-      </main>
+    <div>
+      <TopNavBar />
+      <SideBar />
+      <Lander />
+      <Footer/>
+    </div>
+ 
   );
 }
 
 
-
+ 
 export default App;
