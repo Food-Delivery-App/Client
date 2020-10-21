e133aec8aca95ac2f2e8cc884514184c75e1364f
Venkatesh S <contactvenkatesh17@gmail.com>
2020-10-21 15:48:42 +0530

    balaji changes comit

--- src/layouts/orderLayouts.jsx
+++ src/layouts/orderLayouts.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 import TopNavBar from '../Components/topNavbar/topNavbar';
 import Sidebar from "../Components/sidebar/sidebar";
 import TopBar from "../Components/topNav_2/topNav_2";
-import TopbarThree from "../Components/topNav_3/topNav_3";
+import TopBarTabView from "../Components/topNav_3/topNav_3";
 
 const orderLayout = ({ children }) => (
     <React.Fragment>
         <TopNavBar />
         <TopBar />
-        <TopbarThree />
+        <TopBarTabView />
         <Sidebar />
         {children}
     </React.Fragment>
